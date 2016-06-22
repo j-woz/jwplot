1d0d6105d3dbac7df495fb3c8a5e19f617a3e497
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-06-22 14:49:15 -0500

    Drop CSS

--- make-doc.sh
+++ make-doc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-asciidoc --attribute stylesheet=$PWD/index.css \
-         -a max-width=750px -a textwidth=80 index.txt
+# stylesheet=$PWD/index.css
+asciidoc -a max-width=750px -a textwidth=80 index.txt
