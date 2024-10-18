79e3c69aedbf4b82d187a09e8002b476fcdb2e2f
Justin Wozniak <woz@anl.gov>
2024-10-18 17:00:33 -0500

    Note about EOLs

--- make-doc.sh
+++ make-doc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 2024-10-18: Asciidoc/Python may produce Mac end-of-line characters.
+#             Ignore this.
+
 asciidoc -a stylesheet=$PWD/index.css \
          -a max-width=750px \
          -a textwidth=80 \
